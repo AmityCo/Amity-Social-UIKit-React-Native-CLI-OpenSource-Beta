197684bcbd7ef5bbf449834386a6ec5d61f85341
ptchaya_p <ptchaya.p@gmail.com>
2024-03-29 01:04:23 +0700

    fix: refactor code

--- src/screens/LivestreamRecordedPlayer/index.tsx
+++ src/screens/LivestreamRecordedPlayer/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Alert, View } from 'react-native';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { RootStackParamList } from '../../routes/RouteParamList';
 import Video from 'react-native-video';
 import { useStyles } from './styles';
@@ -9,8 +9,18 @@ const LivestreamRecordedPlayer = () => {
   const route =
     useRoute<RouteProp<RootStackParamList, 'LivestreamRecordedPlayer'>>();
   const { source } = route.params;
+  const navigation = useNavigation();
   const videoRef = useRef(null);
   const styles = useStyles();
+
+  const handleError = () => {
+    Alert.alert('Error while playing video', undefined, [
+      {
+        text: 'OK',
+        onPress: () => navigation.goBack(),
+      },
+    ]);
+  };
 
   return (
     <View style={styles.container}>
@@ -27,12 +37,8 @@ const LivestreamRecordedPlayer = () => {
           bufferForPlaybackMs: 1500,
           bufferForPlaybackAfterRebufferMs: 1500,
         }}
-        onError={() => {
-          Alert.alert('Error while playing video');
-        }}
-        onVideoError={() => {
-          Alert.alert('Error while playing video');
-        }}
+        onError={handleError}
+        onVideoError={handleError}
       />
     </View>
   );
