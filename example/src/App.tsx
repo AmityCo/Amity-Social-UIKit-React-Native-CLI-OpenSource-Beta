dffcd5eb341bb3b8d32bc508ff64d3885fdf16d6
ptchaya_p <ptchaya.p@gmail.com>
2024-06-10 17:56:25 +0700

    fix: revert change on App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,11 +9,11 @@ export default function App() {
   return (
     <AmityUiKitProvider
       configs={config} //put your config json object
-      apiKey="b0efe90c3bdda2304d628918520c1688845889e4bc363d2c" // Put your apiKey
-      apiRegion="staging" // Put your apiRegion
-      userId="Peak" // Put your UserId
-      displayName="Peak" // Put your displayName
-      apiEndpoint="https://api.staging.amity.co" //"https://api.{apiRegion}.amity.co"
+      apiKey="YOUR_API_KEY" // Put your apiKey
+      apiRegion="API_REGION" // Put your apiRegion
+      userId="USER_ID" // Put your UserId
+      displayName="DISPLAYNAME" // Put your displayName
+      apiEndpoint="API_ENDPOINT" //"https://api.{apiRegion}.amity.co"
     >
       <AmityUiKitSocial />
     </AmityUiKitProvider>
